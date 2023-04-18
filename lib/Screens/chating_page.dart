fd3082cc2a479e7c4647aaa297439c569f4fc464
abhishek2355 <abhishekraut2355@gmail.com>
2023-04-18 22:43:16 +0530

    Added input field for chatting page

--- lib/Screens/chating_page.dart
+++ lib/Screens/chating_page.dart
@@ -1,6 +1,8 @@
+import 'package:chat_app/main.dart';
 import 'package:chat_app/model/chat_user.dart';
 import 'package:flutter/material.dart';
 
+import '../helper/chat_input.dart';
 import '../helper/chattingpage_appbar.dart';
 
 class ChattingPage extends StatefulWidget {
@@ -14,13 +16,19 @@ class ChattingPage extends StatefulWidget {
 class _ChattingPageState extends State<ChattingPage> {
   @override
   Widget build(BuildContext context) {
+    media = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          flexibleSpace: ChattingAppbar(widget: widget),
-        ),
-      ),
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(media.height * 70 / 926),
+            child: AppBar(
+              automaticallyImplyLeading: false,
+              flexibleSpace: ChattingAppbar(widget: widget),
+            ),
+          ),
+          body: Column(
+            children: const [ChatInput()],
+          )),
     );
   }
 }
